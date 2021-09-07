eaa38658750e84e7224300be7b59a3f84f3eb8bd
ima18h <ima18h@gmail.com>
2021-09-07 22:45:12 +0200

    Change recursive factorial() to iterative
    Add exceptions for undefined values ni factorial()

--- calculator.py
+++ calculator.py
@@ -5,7 +5,21 @@ def divide(n, d):
     return n / d
 
 def factorial(n):
-    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+    try:
+        n*(n-1)
+    except:
+        raise ValueError("Not defined. choose an integer > 0")
+    if n < 0:
+        raise ValueError("Not defined. choose a number > 0")
+
+    if n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
 
 def sin(x, N = 20):
     sin = 0
