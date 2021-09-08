2f5dc29dca1cb8d5fb2ff022c42e5c09ea5c3535
ima18h <ima18h@gmail.com>
2021-09-08 11:14:12 +0200

    Replace old tests with parametrize from pytest

--- test_calculator.py
+++ test_calculator.py
@@ -1,36 +1,36 @@
 from calculator import add, divide, factorial, sin
-from pytest import approx, raises
+import pytest
 from math import pi, sqrt
 
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(0.1, 0.2) == approx(0.3)
+@pytest.mark.parametrize("arg, arg1, output", [(1, 2, 3), (0.1, 0.2, pytest.approx(0.3))])
+def test_add(arg, arg1, output):
+    assert add(arg, arg1) == output
 
-def test_divide():
-    assert divide(4, 2) == 2
-    assert divide(1, 3) == approx(0.3333333333333333)
+@pytest.mark.parametrize("arg, arg1, output", [(4, 2, 2), (1, 3, pytest.approx(0.3333333333333333))])
+def test_divide(arg, arg1, output):
+    assert divide(arg, arg1) == output
 
-def test_factorial():
-    assert factorial(5) == 120
-    assert factorial(1) == 1
-    assert factorial(2) == 2
+@pytest.mark.parametrize("arg, output", [(1, 1), (2, 2), (3, 6), (5, 120), (0, 1)])
+def test_factorial(arg, output):
+    assert factorial(arg) == output
 
-def test_sin():
-    assert sin(0) == approx(0)
-    assert sin(pi/4) == approx(1/sqrt(2))
-    assert sin(pi/2) == approx(1)
-    assert sin(3*pi/2) == approx(-1)
+# TODO: wtf?
+@pytest.mark.parametrize("arg, output", [(sin(0), pytest.approx(0))])
+def test_sin(arg, output):
+    assert sin(arg) == output
+    assert sin(pi/4) == pytest.approx(1/sqrt(2))
+    assert sin(pi/2) == pytest.approx(1)
+    assert sin(3*pi/2) == pytest.approx(-1)
 
 def test_is_float_raises_ValueError_for_string_arguments():
-    with raises(ValueError):
+    with pytest.raises(ValueError):
         factorial(-1)
 
 def test_factorial_raises_TypeError():
-    with raises(TypeError):
+    with pytest.raises(TypeError):
         factorial(1/3)
 
 def test_divide_raises_ZeroDivisionError():
-    with raises(ZeroDivisionError):
+    with pytest.raises(ZeroDivisionError):
         divide(1, 0)
-
